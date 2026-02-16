ebde7232367d827602fb06b3d68ea49b95992ecd
Bharath <bharath@junixhq.com>
2026-02-16 13:50:17 +0530

    fix(web): add explicit remote image patterns and disable unoptimized mode temporarily

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,10 +3,31 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@fashiondeck/types", "@fashiondeck/utils"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "rukminim2.flixcart.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.myntassets.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.zara.net",
+      },
+      {
+        protocol: "https",
+        hostname: "lp2.hm.com",
       },
     ],
   },
